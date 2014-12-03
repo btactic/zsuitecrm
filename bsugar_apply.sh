89d085938ac133ae1a927d3ddd41ab0cdff033a1
Adrian Gibanel bTactic zimbradev <adrian.gibanel@btactic.com>
2014-12-03 19:27:25 +0100

    Find and replace functionality was put into a function

--- bsugar_apply.sh
+++ bsugar_apply.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-find . -type f -not -name $(basename ${0}) -a -not -iwholename '*.git*'  -print0 | xargs -0 sed -i 's/com_irontec_zsugarH/com_btactic_bsugarH/g'
+set -x
+set -v
+
+# $1: To find
+# $2: To replace
+
+find_and_replace () {
+  find_string="$1"
+  find_replace="$2"
+
+  find . -type f -not -name $(basename ${0}) -a -not -iwholename '*.git*'  -print0 | xargs -0 sed -i 's/'"${find_string}"'/'"${find_replace}"'/g'
+
+}
+
+find_and_replace "com_irontec_zsugarH" "com_btactic_bsugarH"
+
 
