4fbf3bd889bb7b0c355661a374367e79fc15c188
Adrian Gibanel bTactic zimbradev <adrian.gibanel@btactic.com>
2014-12-03 20:39:27 +0100

    Handle design specific bits for rebranding

--- bsugar_apply.sh
+++ bsugar_apply.sh
@@ -24,3 +24,31 @@ for nfile in com_irontec_zsugar* ; do
 done
 
 
+# Find and replace design
+
+find_and_replace "zsugar_container" "bsugar-zsugar_container"
+find_and_replace "zsugar_container" "bsugar-zsugar_container"
+find_and_replace "zsugar_atts" "bsugar-zsugar_atts"
+find_and_replace "zsugar_container" "bsugar-zsugar_container"
+find_and_replace "zsugar_powered_logo" "bsugar-zsugar_powered_logo"
+find_and_replace "zsugar_contactIden" "bsugar-zsugar_contactIden"
+find_and_replace "zsugar_atts" "bsugar-zsugar_atts"
+find_and_replace "zsugar_atts" "bsugar-zsugar_atts"
+find_and_replace "zsugar_logo" "bsugar-zsugar_logo"
+find_and_replace "zsuguar_ilogo" "bsugar-zsuguar_ilogo"
+find_and_replace "zsugar_blogo" "bsugar-zsugar_blogo"
+find_and_replace "zsugar_powered" "bsugar-zsugar_powered"
+
+find_and_replace "ISUGAR-panelIcon" "bsugar-ISUGAR-panelIcon"
+find_and_replace "ISUGAR-icon-right" "bsugar-ISUGAR-icon-right"
+find_and_replace "ISUGAR-lead" "bsugar-ISUGAR-lead"
+find_and_replace "center" "bsugar-center"
+find_and_replace "big" "bsugar-big"
+find_and_replace "med" "bsugar-med"
+find_and_replace "marTop" "bsugar-marTop"
+find_and_replace "MoreButtons" "bsugar-MoreButtons"
+
+
+find_and_replace "zsugar.css" "bsugar-zsugar.css"
+mv zsugar.css bsugar-zsugar.css
+
